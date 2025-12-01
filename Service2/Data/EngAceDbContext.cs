fcce7b812de1929191b9b79bc0d24a7f869e6111
Sotatek-congvu <cong.vu@sotatek.com>
2025-12-01 18:56:55 +0700

    update server2

--- Service2/Data/EngAceDbContext.cs
+++ Service2/Data/EngAceDbContext.cs
@@ -40,7 +40,7 @@ public partial class EngAceDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("");
+        => optionsBuilder.UseNpgsql("Host=aws-1-ap-southeast-1.pooler.supabase.com;Port=5432;Database=postgres;Username=postgres.nitqneodiyligoydzyne;Password=Congkcs12345@@@;SSL Mode=Require;Trust Server Certificate=true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
